6d108f7beada06836e2cf4ae67f4576d684bc3b8
Helmasur <helmasur.king@gmail.com>
2018-06-24 10:28:54 +0200

    kivy begin from laptop

---
+++ main.py
@@ -0,0 +1,25 @@
+#Kivy v1.10.0
+#Python v3.6.5
+
+'''
+Note
+COMMON ERROR: The name of the kv file, e.g. pong.kv,
+must match the name of the app, e.g. PongApp
+(the part before the App ending).
+'''
+
+from kivy.app import App
+from kivy.uix.widget import Widget
+
+
+class MainFrame(Widget):
+    pass
+
+
+class LayersApp(App):
+    def build(self):
+        return MainFrame()
+
+
+if __name__ == '__main__':
+    LayersApp().run()
